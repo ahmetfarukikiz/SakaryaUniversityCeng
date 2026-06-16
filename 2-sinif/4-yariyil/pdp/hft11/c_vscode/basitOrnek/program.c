06183b5ad9a4c411d2349f5994500c52f25253fa
ahmetfarukikiz <ahmetfarukikiz41@gmail.com>
2026-06-17 01:21:29 +0300

    hft 11 basitOrnek'e ufak ekleme

--- 2-sinif/4-yariyil/pdp/hft11/c_vscode/basitOrnek/program.c
+++ 2-sinif/4-yariyil/pdp/hft11/c_vscode/basitOrnek/program.c
@@ -1,18 +1,17 @@
 #include "stdio.h"
 #include "setjmp.h"
 
-typedef enum {false, true}boolean;
-
-jmp_buf jumper;
+jmp_buf jumper; //içinde cpu bilgilerini tutan bir buffer
 
 int bolme(int x, int y){
-	if(y == 0) longjmp(jumper, 1); //jumper'ın değerini 1 yap
+	// y 0'a eşit ise jumper'ın içinde tutulan adrese git. (setjmp satırına döner)
+	if(y == 0) longjmp(jumper, 1); //setjmp'ın yeni döndüreceği değer 1 olacak
 	return x/y;
 }
 
 int main(){
 	int a = 10, b=0;
-	if(setjmp(jumper) == false){ // geri dönme noktası bırak ve değeri değiştiyse geri dön.
+	if(setjmp(jumper) == 0){ //jumper'ın içine şuanki satırı kaydeder ve ilk kez 0 döndürür
 		printf("%d",bolme(a,b));
 	}
 	else{
@@ -20,3 +19,4 @@ int main(){
 	}
 	return 0;
 }
+
